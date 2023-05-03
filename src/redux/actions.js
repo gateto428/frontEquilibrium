71289caac312f15f48603e1ef10a8de860616ebc
Nicolas Suarez <2jmk3218@gmail.com>
2023-05-02 19:09:24 -0500

    prueba

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -10,7 +10,7 @@ export const GET_PLAN = 'GET_PLAN';
 export const GET_TOTAL_PLAN = 'GET_TOTAL_PLAN';
 export const GET_TOTAL_USERS = 'GET_TOTAL_USERS';
 export const ERROR = 'ERROR';
-const HOST  = 'https://3.144.43.47:8080/api/v1/equilibrium'
+const HOST  = 'http://3.144.43.47:8080/api/v1/equilibrium'
 
 
 
